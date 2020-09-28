a45828b7bc271fc70cb279f1f37c2ca19ba3a2ce
Leandro Matayoshi <lmatayoshi@manas.com.ar>
2020-09-27 23:14:45 -0300

    Keep order when saving JSON dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,6 @@
 import json
 import random
+from collections import OrderedDict
 
 DICTIONARY_FILENAME = "./dictionary.json"
 
@@ -31,16 +32,27 @@ def update_score(expr, value):
   save_dictionary(dictionary)
 
 def print_dictionary_entries(entries):
-  print '\n'
+  print('\n')
   for e in entries:
     print ('  Expression:  %s  ' %(e["expression"].capitalize()))
     print ('  Context:  %s  ' %(e["context"]))
     print ('  Meaning:  %s  ' %(e["meaning"]))
     print ('  Example:  %s  ' %(e["example"]))
-    print '\n'
+    print('\n')
 
 def save_dictionary(dictionary):
+  ordered_dict = list(map(lambda entry: OrderedDict(
+    [
+      ("expression", entry["expression"]),
+      ("context", entry["context"]),
+      ("meaning", entry["meaning"]),
+      ("example", entry["example"]),
+      ("extra", entry["extra"]),
+      ("keywords", entry["keywords"]),
+      ("score", entry["score"]),
+    ]
+  ), dictionary))
   with open(DICTIONARY_FILENAME, 'w') as file:
-    json.dump(dictionary, file)
+    json.dump(ordered_dict, file, indent=4)
 
 ## Al final de test, VOLVER A ESCRIBIR el JSON en el archivo
--- main.py
+++ main.py
@@ -4,27 +4,27 @@ from dictionary import find_random_expression, definition, print_dictionary_entr
 def test_me_flow(score):
   random_expression = find_random_expression(score)
   print ('\n  %s  \n' %(random_expression))
-  result = raw_input("Correct?: (y)es / (n)o   ").upper()
+  result = input("Correct?: (y)es / (n)o   ").upper()
   if result[0] == 'Y':
     update_score(random_expression, 1)
-    print "Well done! Your score is +1"
+    print("Well done! Your score is +1")
   elif result[0] == 'N':
     update_score(random_expression, -1)
-    print "Let's keep practising! Your score is -1"
+    print("Let's keep practising! Your score is -1")
   else:
-    print "Invalid result"
+    print("Invalid result")
 
 argv = sys.argv[1:]
 
 if (len(argv) < 1):
-  print "Options:'test [level]' or 'remind [expression]'"
+  print("Options:'test [level]' or 'remind [expression]'")
 elif (argv[0] == 'test'):
   if len(argv) < 2: 
-    print "Missing level on testing expression"
+    print("Missing level on testing expression")
   else:
     test_me_flow(argv[1])
 elif (argv[0] == 'definition'):
   keyword = argv[2] if len(argv) > 1 else None
   print_dictionary_entries(definition(argv[1], keyword))
 else:
-  print "Options:'test [level]' or 'remind [expression]'"
+  print("Options:'test [level]' or 'remind [expression]'")
